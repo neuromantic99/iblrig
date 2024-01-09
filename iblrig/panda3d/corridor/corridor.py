4bd5830e8b6df2e6549bc156cf1ec4ad734be8d7
neuromantic99 <jrowlandm@gmail.com>
2024-01-09 15:53:43 +0000

    add textures from disseroth lab paper

--- iblrig/panda3d/corridor/corridor.py
+++ iblrig/panda3d/corridor/corridor.py
@@ -1,4 +1,5 @@
 import builtins
+from typing import Any
 
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
@@ -6,13 +7,14 @@ from panda3d.core import (
     CardMaker,
     KeyboardButton,
     WindowProperties,
+    TextureStage,
 )
 
 NUM_TURNS_PER_LAP = 2
 
 CORRIDOR_LENGTH = 200
-CORRIDOR_WIDTH = 40
-CORRIDOR_HEIGHT = 10
+CORRIDOR_WIDTH = 25
+CORRIDOR_HEIGHT = 25
 
 CAMERA_HEIGHT = 5
 # +1 so that the camera is not half out of the back wall
@@ -32,7 +34,8 @@ class Corridor(ShowBase):
         props = WindowProperties()
         props.setSize(800, 600)
 
-        self.build_corridor()
+        self.build_corridor(0, "blueTriangles.jpg", False)
+        self.build_corridor(CORRIDOR_LENGTH, "pinkBars.png", True)
 
         self.left_window = self.openWindow(props, makeCamera=0)
         self.right_window = self.openWindow(props, makeCamera=0)
@@ -52,11 +55,11 @@ class Corridor(ShowBase):
 
         self.taskMgr.add(self.moveCameraTask, "MoveCameraTask")
 
-    def start(self):
+    def start(self) -> None:
         if not self.win:
             self.openMainWindow()
 
-    def set_camera_position(self, fraction_through_turn: float):
+    def set_camera_position(self, fraction_through_turn: float) -> None:
         """Set camera position based on the number of
         full wheel rotations required to complete the corridor
         TODO: This logic might be slightly odd because it completely ignores
@@ -65,11 +68,11 @@ class Corridor(ShowBase):
         distance = fraction_through_turn * CORRIDOR_LENGTH / NUM_TURNS_PER_LAP
         self.camera.setPos(0, distance + CAMERA_START_Y, CAMERA_HEIGHT)
 
-    def step(self):
+    def step(self) -> None:
         self.taskMgr.step()
 
-    def moveCameraTask(self, task):
-        speed = 10
+    def moveCameraTask(self, task: Any) -> Task.cont:
+        speed = 400
         dt = globalClock.getDt()  # Get the actual delta time
 
         if self.mouseWatcherNode.is_button_down(KeyboardButton.up()):
@@ -79,9 +82,12 @@ class Corridor(ShowBase):
 
         return Task.cont
 
-    def build_corridor(self):
+    def build_corridor(
+        self, y_offset: int, wall_texture: str, add_back_wall: bool
+    ) -> None:
         cm = CardMaker("corridor_segment")
-        textures = ["checkers.jpg", "floor.jpg", "checkers.jpg", "horGrat.jpg"]
+
+        corridor = {}
 
         # Top and bottom
         cm.setFrame(
@@ -90,15 +96,13 @@ class Corridor(ShowBase):
             -CORRIDOR_LENGTH / 2,
             CORRIDOR_LENGTH / 2,
         )
-        floor = self.render.attachNewNode(cm.generate())
-        floor.setPos(0, 0, 0)
-        floor.setP(-90)
-        floor.setTwoSided(True)
+        corridor["floor"] = self.render.attachNewNode(cm.generate())
+        corridor["floor"].setPos(0, y_offset, 0)
+        corridor["floor"].setP(-90)
 
-        ceiling = self.render.attachNewNode(cm.generate())
-        ceiling.setPos(0, 0, CORRIDOR_HEIGHT)
-        ceiling.setP(-90)
-        ceiling.setTwoSided(True)
+        corridor["ceiling"] = self.render.attachNewNode(cm.generate())
+        corridor["ceiling"].setPos(0, y_offset, CORRIDOR_HEIGHT)
+        corridor["ceiling"].setP(-90)
 
         # Side Walls
         cm.setFrame(
@@ -108,15 +112,13 @@ class Corridor(ShowBase):
             CORRIDOR_HEIGHT / 2,
         )
 
-        left_wall = self.render.attachNewNode(cm.generate())
-        left_wall.setPos(-CORRIDOR_WIDTH / 2, 0, CORRIDOR_HEIGHT / 2)
-        left_wall.setH(90)
-        left_wall.setTwoSided(True)
+        corridor["left_wall"] = self.render.attachNewNode(cm.generate())
+        corridor["left_wall"].setPos(-CORRIDOR_WIDTH / 2, y_offset, CORRIDOR_HEIGHT / 2)
+        corridor["left_wall"].setH(90)
 
-        right_wall = self.render.attachNewNode(cm.generate())
-        right_wall.setPos(CORRIDOR_WIDTH / 2, 0, CORRIDOR_HEIGHT / 2)
-        right_wall.setH(-90)
-        right_wall.setTwoSided(True)
+        corridor["right_wall"] = self.render.attachNewNode(cm.generate())
+        corridor["right_wall"].setPos(CORRIDOR_WIDTH / 2, y_offset, CORRIDOR_HEIGHT / 2)
+        corridor["right_wall"].setH(-90)
 
         # Front and back
         cm.setFrame(
@@ -126,26 +128,40 @@ class Corridor(ShowBase):
             CORRIDOR_HEIGHT / 2,
         )
 
-        front_wall = self.render.attachNewNode(cm.generate())
-        front_wall.setPos(0, CORRIDOR_LENGTH / 2, CORRIDOR_HEIGHT / 2)
-        front_wall.setH(180)
-        front_wall.setTwoSided(True)
+        if add_back_wall:
+            corridor["back_wall"] = self.render.attachNewNode(cm.generate())
+            corridor["back_wall"].setPos(
+                0, CORRIDOR_LENGTH / 2 + y_offset, CORRIDOR_HEIGHT / 2
+            )
+            corridor["back_wall"].setH(180)
 
-        back_wall = self.render.attachNewNode(cm.generate())
-        back_wall.setPos(0, -CORRIDOR_LENGTH / 2, CORRIDOR_HEIGHT / 2)
-        back_wall.setTwoSided(True)
+        # corridor["front_wall"] = self.render.attachNewNode(cm.generate())
+        # corridor["front_wall"].setPos(
+        #     0, -CORRIDOR_LENGTH / 2 + y_offset, CORRIDOR_HEIGHT / 2
+        # )
 
-        for idx, model in enumerate(
-            [ceiling, floor, left_wall, right_wall, front_wall, back_wall]
-        ):
+        for model_name, model in corridor.items():
+            texture_name = (
+                "floor.jpg"
+                if model_name == "floor"
+                else wall_texture
+                if model_name in ["left_wall", "right_wall"]
+                else "checkers.jpg"
+            )
             texture = self.loader.load_texture(
-                f"iblrig/panda3d/corridor/textures/{textures[idx % 4]}"
+                f"iblrig/panda3d/corridor/textures/{texture_name}"
             )
 
             model.setTexture(texture, 1)
+            model.setTwoSided(True)
             model.reparentTo(self.render)
 
-            # model.setTexScale(TextureStage.getDefault(), 100, 10)
+            num_texture_tiles = int(CORRIDOR_LENGTH / CORRIDOR_HEIGHT)
+
+            if model_name in ["floor", "ceiling"]:
+                model.setTexScale(TextureStage.getDefault(), 1, num_texture_tiles)
+            elif model_name in ["left_wall", "right_wall"]:
+                model.setTexScale(TextureStage.getDefault(), num_texture_tiles, 1)
 
 
 if __name__ == "__main__":
