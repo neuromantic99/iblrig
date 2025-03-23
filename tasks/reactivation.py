0fc4d5c3df58a4758b9b176ef93b93c6e58811f7
josefbitzenhofer <josef.bitzenhofer@icloud.com>
2025-03-23 11:15:58 +0000

    added reactivation session to task to store rotary encoder and licks

---
+++ tasks/reactivation.py
@@ -0,0 +1,98 @@
+import datetime
+from matplotlib import pyplot as plt
+from path_helper import path_helper
+import random
+
+path_helper()
+
+from pathlib import Path
+from typing import List
+
+from tasks.habituation_task import IblBase
+
+
+from iblutil.util import setup_logger
+from pybpodapi.protocol import StateMachine
+
+from iblrig.hardware import SOFTCODE
+from iblutil.spacer import Spacer
+
+
+import yaml
+
+from iblrig.panda3d.corridor.corridor import Corridor
+
+
+log = setup_logger("iblrig")
+
+
+with open(Path(__file__).parent.joinpath("subject_parameters.yaml")) as f:
+    SUBJECT_PARAMETERS = yaml.safe_load(f)
+
+
+class Session(IblBase):
+    def __init__(self, subject: str) -> None:
+        self.protocol_name = "Reactivation session"
+
+        super().__init__(subject=subject)
+        self.rotary_encoder_position: List[float] = []
+        self.injection_rotary_encoder_position(self.rotary_encoder_position)
+
+        self.start_time = datetime.datetime.now()
+
+    def start_bpod(self):
+        self.run()
+
+    def get_state_machine_trial(self, i):
+        sma = StateMachine(self.bpod)
+
+        Spacer(n_pulses=random.choice([1, 2, 3, 4, 5]), tup=0.01).add_spacer_states(
+            sma, "trial_start"
+        )
+
+        session_duration = (
+            self.task_params["SESSION_LENGTH"] * 60
+        )  # TODO: check if this is correct
+
+        sma.set_global_timer(1, session_duration)
+
+        sma.add_state(
+            state_name="trial_start",
+            state_timer=0,
+            state_change_conditions={"Tup": "reset_rotary_encoder"},
+            output_actions=[
+                ("GlobalTimerTrig", 1),
+            ],
+        )
+
+        sma.add_state(
+            state_name="reset_rotary_encoder",
+            state_timer=0,
+            output_actions=[self.bpod.actions.rotary_encoder_reset],
+            state_change_conditions={"Tup": "store_encoder_position"},
+        )
+
+        sma.add_state(
+            state_name="store_encoder_position",
+            state_timer=0,
+            output_actions=[("SoftCode", SOFTCODE.STORE_ENCODER_POSITION)],
+            state_change_conditions={"Tup": "transition"},
+        )
+
+        sma.add_state(
+            state_name="transition",
+            state_timer=1 / 10,
+            state_change_conditions={
+                "GlobalTimer1_End": "exit",
+                "Tup": "store_encoder_position",
+            },
+        )
+
+        return sma
+
+
+if __name__ == "__main__":  # pragma: no cover
+    session = Session(SUBJECT_PARAMETERS["subject_id"])
+    session.start_bpod()
+    session.device_rotary_encoder.rotary_encoder.disable_stream()
+    session.device_rotary_encoder.rotary_encoder.close()
